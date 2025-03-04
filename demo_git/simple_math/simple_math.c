6c50df75cdca113f67da1d5782e2c2cb479df19f
lorisscostanzo <costanzoloris@gmail.com>
2025-03-04 12:16:52 +0100

    with complex number tested

--- demo_git/simple_math/simple_math.c
+++ demo_git/simple_math/simple_math.c
@@ -8,10 +8,9 @@
 cx_float_t cx_mean(cx_int_t a, cx_int_t b)
 {
     cx_float_t average;
-    average.re = (a.re + b.re) / 2;
-    average.imm = (a.imm + b.imm) / 2;
+    average.re = mean(a.re, b.re);
+    average.imm = mean(a.imm, b.imm);
     return average;
-
 }
 
 float mean(int a, int b)
